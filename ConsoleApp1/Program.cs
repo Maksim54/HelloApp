f849040c05d0246db4aa008086f109b6d943016c
opilane <opilane@HP-CZC6297B8C>
2022-09-16 11:30:29 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using (ApplicationContext db = new ApplicationContext())
+{
+    // создаем два объекта User
+    User tom = new User { Name = "Tom", Age = 33 };
+    User alice = new User { Name = "Alice", Age = 26 };
+
+    // добавляем их в бд
+    db.Users.Add(tom);
+    db.Users.Add(alice);
+    db.SaveChanges();
+    Console.WriteLine("Objects saved successfully");
+
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("List of objects:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
